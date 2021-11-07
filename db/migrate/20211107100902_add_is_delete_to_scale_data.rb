9b037151dedbacc983a374614258f3d13171966f
Tomato-Hakari <teraken_terao@icloud.com>
2021-11-07 19:40:57 +0900

    Update 20211107100902_add_is_delete_to_scale_data.rb

--- db/migrate/20211107100902_add_is_delete_to_scale_data.rb
+++ db/migrate/20211107100902_add_is_delete_to_scale_data.rb
@@ -1,5 +1,5 @@
 class AddIsDeleteToScaleData < ActiveRecord::Migration[6.1]
   def change
-    add_column :scale_data, :isDelete, :boolean
+    add_column :scale_data, :isDelete, :boolean, null: false, default: true
   end
 end
