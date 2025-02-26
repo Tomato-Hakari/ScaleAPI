f62c4733700f037ba819342285a5eb937b0cf2d7
yokochi <91511674+Yokochi513@users.noreply.github.com>
2025-02-26 11:33:21 +0900

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :scale_data
   post '/index', to: 'dbupdates#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # ルートパスを設定（例: ScaleDataController の index をトップページにする）
+  root 'scale_data#index'
 end
